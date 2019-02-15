70ee195b8deaaed4bb2c94590ad20c9d1c416966
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-15 22:57:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,7 @@ def my_collect(array)
   i = 0 
   new_array = []
   while i < array.length 
-  yield array[i]
-  new_array << array[i]
+  yield {new_array << array[i]}
   i+=1 
 end
 return new_array
