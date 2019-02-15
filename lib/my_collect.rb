c421a7326430f8879e7ba71a8b5cabeba0095876
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-15 22:59:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+testing_array = []
 
+def my_collect(array)
+  i = 0 
+  new_array = []
+  while i < array.length 
+  new_value = yield array[i]
+  new_array << new_value
+  i+=1 
+end
+return new_array
+end
 
